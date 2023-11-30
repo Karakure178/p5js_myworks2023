fcd98dc7d33d0e02fdc8b0f08989abf16d24f810
Karakure178 <egg9017shun@gmail.com>
2023-12-01 00:30:51 +0900

    fix - vector.jsの修正&アニメーション動くよう修正

--- src/mocks/pixi_test1/js/libs/circle.js
+++ src/mocks/pixi_test1/js/libs/circle.js
@@ -1,21 +1,20 @@
-import { Vector ,Vector.leap } from './utils/vector';
+import { Vector } from './utils/vector';
 import { map } from './utils/map';
 import { radians } from './utils/radians';
 
 // 2つの円を行ったり来たりさせるアニメーション
 // app: pixiアプリケーション
 // circle1: 円を描画するためのpixi.jsのGraphicsクラス
-export const circleVector = (app, circle1) => {
-  app.stage.addChild(circle1);
 
-  // 円上に配置するための処理
-  const circle_container = [];
-  const interpolatedData = []; // 線形補間用箱
-  const interpolatedData_num = 20; // 線形補間の分割数
-  const points_num = 10; // 配置する小さな円の個数
-  const r = 360 / points_num; // 小さな円を配置する角度
-  const r_container = [50, 150, 170]; // 配置する円の半径
+// 円上に配置するための処理
+const circle_container = [];
+export const interpolatedData = []; // 線形補間用箱
+export const interpolatedData_num = 20; // 線形補間の分割数
+export const points_num = 50; // 配置する小さな円の個数
+const r = 360 / points_num; // 小さな円を配置する角度
+const r_container = [80, 150, 270]; // 配置する円の半径
 
+export const circleVector = () => {
   for (let k = 0; k < r_container.length; k++) {
     circle_container.push([]);
     for (let i = 0; i < points_num; i++) {
@@ -27,19 +26,20 @@ export const circleVector = (app, circle1) => {
 
   for (let n = 0; n < interpolatedData_num; n++) {
     // 線形補間を入れるために配列宣言
-    interpolatedData.push(new Array(points_num).fill(new Vector()));
+    interpolatedData.push(new Array(points_num).fill(new Vector(0, 0)));
     for (let i = 0; i < points_num; i++) {
-      // TODO vector.lerp要修正
-      const lerp = Vector.lerp(circle_container[0][i], circle_container[2][i], n / interpolatedData_num);
+      //console.log('n / interpolatedData_num:', n / interpolatedData_num);
+      const lerp = new Vector(0, 0).lerp(circle_container[0][i], circle_container[2][i], n / interpolatedData_num);
       interpolatedData[n][i] = new Vector(lerp.x, lerp.y);
     }
   }
+};
 
+export const circleVectorDraw = (circle1, count) => {
   //実際に小さい円と大きい円の間をいったり来たりさせる
-  const count = 0;
+  circle1.clear();
   for (let n = 0; n < points_num; n++) {
     circle1.beginFill(0xffffff);
-    circle1.drawCircle(interpolatedData[count][n].x, interpolatedData[count][n].y, 50);
-    // p.circle(,  (count + 5) * 2);
+    circle1.drawCircle(interpolatedData[count][n].x, interpolatedData[count][n].y, (5 * count) / 3);
   }
 };
--- src/mocks/pixi_test1/js/libs/sketch.js
+++ src/mocks/pixi_test1/js/libs/sketch.js
@@ -1,26 +1,46 @@
+import gsap from 'gsap';
 import * as PIXI from 'pixi.js';
-import { circleVector } from './circle';
+import { circleVector, circleVectorDraw, interpolatedData_num } from './circle';
 
 // pixiを使ったサンプル作成
 export const sketch = () => {
   let app = []; // pixiアプリケーションを格納する変数
+  let circle1; // 円を描画するためのpixi.jsのGraphicsクラス
   const canvas = document.getElementById('canvas'); // canvas要素を取得
+  const frame = { count: 0 }; // モーション用のカウンター
 
   // setup関数とdraw関数を定義
-  const setup = (app) => {
+  const setup = () => {
     app = new PIXI.Application(); // pixiアプリケーションを作成
     app.renderer.resize(800, 600); // キャンバスサイズを指定
     app.renderer.background.color = 0x061639; // 背景色を指定
     canvas.appendChild(app.view); // canvas要素をDOMに追加
+    globalThis.__PIXI_APP__ = app;
 
-    const circle1 = new PIXI.Graphics();
+    circle1 = new PIXI.Graphics();
+    app.stage.addChild(circle1);
+
     circleVector(app, circle1); // 円を描画
+    motion(frame); // モーションを作成
   };
 
-  const draw = (app) => {
+  const draw = () => {
     // ここに描画処理を記述
+    app.ticker.add(() => {
+      circleVectorDraw(circle1, parseInt(frame.count));
+    });
   };
 
-  setup(app); // pixiアプリケーションをセットアップ
-  draw(app); // pixiアプリケーションを描画
+  setup(); // pixiアプリケーションをセットアップ
+  draw(); // pixiアプリケーションを描画
+};
+
+// gsapを使ったモーション作成
+const motion = (frame) => {
+  // ここにモーション処理を記述
+  gsap.timeline({ repeat: -1 }).to(frame, {
+    count: interpolatedData_num - 1,
+    duration: 3,
+    ease: 'linear',
+  });
 };
